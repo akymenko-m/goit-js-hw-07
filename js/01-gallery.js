f3c8018e443dd1bbca747a2ab9b18585a9eb37f0
Mariia Akymenko <dafnarianna@gmail.com>
2022-12-27 00:50:43 +0200

    make task 1, 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,58 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+//Create and render markup
+const galleryList = document.querySelector('.gallery');
+
+const makeGalleryItem = ({ preview, original, description } = {}) => {
+    return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>
+    `
+};
+
+const galleryItem = galleryItems.map(el => {
+    return makeGalleryItem(el);
+});
+// console.log(galleryItem);
+galleryList.insertAdjacentHTML('beforeend', galleryItem.join(''));
+
+//Implementation of delegation and obtaining the url of a large image
+galleryList.addEventListener('click', openLightboxByClickOfImage);
+
+let instance; //take out the variable to access it in a function
+
+function openLightboxByClickOfImage(event) {
+    event.preventDefault();
+
+    if (!event.target.classList.contains('gallery__image')) {
+        return; 
+    };
+    // console.log(event.target.dataset.source);
+
+    instance = basicLightbox.create(`
+        <img src="${event.target.dataset.source}" alt="${event.target.alt}" />
+    `);
+    instance.show();
+    // console.log(event.target);
+
+    if (basicLightbox.visible()) {
+        document.addEventListener("keydown", closeLightboxByClickOfImage);
+    }
+}
+
+// close large image
+function closeLightboxByClickOfImage(event) {
+    if(event.code === 'Escape') {
+        instance.close();
+    }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+const makeGalleryItem = ({ preview, original, description } = {}) => {
+    return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    `
+};
+
+const galleryItem = galleryItems.map(el => {
+    return makeGalleryItem(el);
+});
+// console.log(galleryItem);
+galleryList.insertAdjacentHTML('beforeend', galleryItem.join(''));
+
+
+galleryList.addEventListener('click', openLightboxByClickOfImage);
+
+ let lightbox = new SimpleLightbox('.gallery a', { captionsData:'alt', captionDelay:250 }); //fadeSpeed:300
+
+function openLightboxByClickOfImage(event) {
+    event.preventDefault();
+}
